6b49db28be513019bf9059a0bf48845983f4b6b8
dat.pl <dat.pl@mbageas.life>
2024-10-05 21:20:21 +0700

    update one to many models

--- src/main/java/com/example/SS2_Backend/model/onetomany/MatchingProblem.java
+++ src/main/java/com/example/SS2_Backend/model/onetomany/MatchingProblem.java
@@ -4,13 +4,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 import org.moeaframework.core.Problem;
 import org.moeaframework.core.Solution;
 import org.moeaframework.core.Variable;
 import org.moeaframework.core.variable.Permutation;
 
 import java.util.*;
+import java.util.function.DoubleUnaryOperator;
+import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
+
+import static com.example.SS2_Backend.util.StringExpressionEvaluator.*;
 
 @Slf4j
 @Data
@@ -171,9 +177,131 @@ public class MatchingProblem implements Problem {
     }
 
     private double withFitnessFunctionEvaluation(double[] satisfactions, String fitnessFunction) {
-        // Implementation similar to StableMatchingProblem
-        // You may need to adapt this method based on your specific requirements
-        return 0.0; // Placeholder
+        StringBuilder tmpSB = new StringBuilder();
+        for (int c = 0; c < fitnessFunction.length(); c++) {
+            char ch = fitnessFunction.charAt(c);
+            if (ch == 'S') {
+                if (Objects.equals(fitnessFunction.substring(c, c + 5), "SIGMA")) {
+                    if (fitnessFunction.charAt(c + 5) != '{') {
+                        System.err.println("Missing '{'");
+                        System.err.println(fitnessFunction);
+                        throw new RuntimeException("Missing '{' after Sigma function");
+                    } else {
+                        int expressionStartIndex = c + 6;
+                        int expressionLength = getSigmaFunctionExpressionLength(fitnessFunction,
+                                expressionStartIndex);
+                        String expression = fitnessFunction.substring(expressionStartIndex,
+                                expressionStartIndex + expressionLength);
+                        double val = sigmaCalculate(satisfactions, expression);
+                        tmpSB.append(convertToStringWithoutScientificNotation(val));
+                        c += expressionLength + 3;
+                    }
+                }
+                // Check for F(index) pattern
+                if (c + 3 < fitnessFunction.length() && fitnessFunction.charAt(c + 1) == '(' &&
+                        fitnessFunction.charAt(c + 3) == ')') {
+                    if (isNumericValue(fitnessFunction.charAt(c + 2))) {
+                        int set = Character.getNumericValue(fitnessFunction.charAt(c + 2));
+                        //Calculate SUM
+                        tmpSB.append(convertToStringWithoutScientificNotation(DoubleStream
+                                .of(getSatisfactoryOfASetByDefault(satisfactions, set))
+                                .sum()));
+                    }
+                }
+                c += 3;
+            } else if (ch == 'M') {
+                int ssLength = AfterTokenLength(fitnessFunction, c);
+                int positionOfM = Integer.parseInt(fitnessFunction.substring(c + 1,
+                        c + 1 + ssLength));
+                if (positionOfM < 0 || positionOfM > individuals.getTotalIndividuals()) {
+                    throw new IllegalArgumentException(
+                            "invalid position after variable M: " + positionOfM);
+                }
+                double valueOfM = satisfactions[positionOfM - 1];
+                tmpSB.append(valueOfM);
+                c += ssLength;
+            } else {
+                //No occurrence of W/w/P/w
+                tmpSB.append(ch);
+            }
+        }
+        System.out.println(tmpSB);
+        return new ExpressionBuilder(tmpSB.toString())
+                .build()
+                .evaluate();
+    }
+
+    private static int getSigmaFunctionExpressionLength(String function, int startIndex) {
+        int num = 0;
+        for (int i = startIndex; i < function.charAt(i); i++) {
+            char ch = function.charAt(i);
+            if (ch == '}') {
+                return num;
+            } else {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    private double sigmaCalculate(double[] satisfactions, String expression) {
+        System.out.println("sigma calculating...");
+        double[] streamValue = null;
+        String regex = null;
+        for (int i = 0; i < expression.length() - 1; i++) {
+            char ch = expression.charAt(i);
+            if (ch == 'S') {
+                char set = expression.charAt(i + 1);
+                switch (set) {
+                    case '1':
+                        streamValue = getSatisfactoryOfASetByDefault(satisfactions, 0);
+                        regex = "S1";
+                        break;
+                    case '2':
+                        streamValue = getSatisfactoryOfASetByDefault(satisfactions, 1);
+                        regex = "S2";
+                        break;
+                    default:
+                        throw new IllegalArgumentException(
+                                "Illegal value after S regex in sigma calculation: " + expression);
+                }
+            }
+        }
+        if (regex == null) {
+            return 0;
+        }
+        Expression exp = new ExpressionBuilder(expression)
+                .variables(regex)
+                .build();
+        String finalRegex = regex;
+        DoubleUnaryOperator calculator = x -> {
+            exp.setVariable(finalRegex, x);
+            return exp.evaluate();
+        };
+        return DoubleStream
+                .of(streamValue)
+                .map(calculator)
+                .sum();
+    }
+
+    private double[] getSatisfactoryOfASetByDefault(double[] Satisfactions, int set) {
+        int numberOfIndividual = individuals.getTotalIndividuals();
+        int numberOfIndividualOfSet0 = individuals.getTotalIndividuals();
+        double[] setSatisfactions;
+        if (set == 0) {
+            setSatisfactions = new double[numberOfIndividualOfSet0];
+            System.arraycopy(Satisfactions, 0, setSatisfactions, 0, numberOfIndividualOfSet0);
+        } else {
+            setSatisfactions = new double[numberOfIndividual - numberOfIndividualOfSet0];
+            if (numberOfIndividual - numberOfIndividualOfSet0 >= 0) {
+                int idx = 0;
+                for (int i = numberOfIndividualOfSet0; i < numberOfIndividual; i++) {
+                    setSatisfactions[idx] = Satisfactions[i];
+                    idx++;
+                }
+            }
+        }
+        return setSatisfactions;
     }
 
     private double[] getAllSatisfactions(Match match) {
@@ -223,5 +351,4 @@ public class MatchingProblem implements Problem {
         // No resources to close
     }
 
-    // Additional helper methods and overrides as needed
 }

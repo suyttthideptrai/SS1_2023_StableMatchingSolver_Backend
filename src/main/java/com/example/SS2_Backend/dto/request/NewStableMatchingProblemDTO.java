74a0ad7e8bc0073fee38f89dd2bcd66962603dd5
Khanh Duy <duykhanh471@protonmail.com>
2024-10-21 14:24:03 +0700

    Modified NewStableMatchingProblem

---
+++ src/main/java/com/example/SS2_Backend/dto/request/NewStableMatchingProblemDTO.java
@@ -0,0 +1,67 @@
+package com.example.SS2_Backend.dto.request;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class NewStableMatchingProblemDTO {
+    private String problemName;
+    private int numberOfSets;
+    private int numberOfIndividuals;
+    private String[] allPropertyNames;
+    private int[] individualSetIndices;
+    private int[] individualCapacities;
+
+    // XU LY REQUIREMENT instead of String??
+    private List<List<String>> individualRequirements;
+    private List<List<Double>> individualWeights;
+    private List<List<Double>> individualProperties;
+
+    private String[] evaluateFunction;
+    private String fitnessFunction;
+    private int populationSize;
+    private int generation;
+    private int maxTime;
+    private String algorithm;
+    private String distributedCores;
+
+    public String toString() {
+        return "Matching_Theory_Problem {" + "\n" +
+                " ProblemName = " + problemName + "\n" +
+                ", NumberOfSets = " + numberOfSets + "\n" +
+                ", NumberOfIndividuals = " + numberOfIndividuals + "\n" +
+                ", IndividualSetIndices = " + Arrays.toString(individualSetIndices) + "\n" +
+                ", IndividualCapacities = " + Arrays.toString(individualCapacities) + "\n" +
+                ", AllPropertyName = " + Arrays.toString(allPropertyNames) +
+                ", fitnessFunction = '" + fitnessFunction + "\n" +
+                ", PopulationSize = " + populationSize + "\n" +
+                ", Generation = " +generation + "\n" +
+                ", individualRequirements: " + Arrays.deepToString(individualRequirements.toArray()) + "\n" +
+                ", individualWeights: " + Arrays.deepToString(individualWeights.toArray()) + "\n" +
+                ", individualProperties: " + Arrays.deepToString(individualProperties.toArray()) + "\n" +
+                "}";
+    }
+
+    public static String[][] fromListToStringArray(List<List<String>> list) {
+        String[][] array = new String[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            List<String> currentArr = list.get(i);
+            array[i] = currentArr.toArray(new String[0]);
+        }
+        return array;
+    }
+
+    public static double[][] fromListToDoubleArray(List<List<Double>> list) {
+        double[][] array = new double[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i).stream().mapToDouble(Double::doubleValue).toArray();
+        }
+        return array;
+    }
+}

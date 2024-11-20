0680236f588681715b63377cf1fbf29f60f8c481
Khanh Duy <duykhanh471@protonmail.com>
2024-11-20 15:51:06 +0700

    Cải thiện StableMatchingDTO

--- src/main/java/com/example/SS2_Backend/dto/request/NewStableMatchingProblemDTO.java
+++ src/main/java/com/example/SS2_Backend/dto/request/NewStableMatchingProblemDTO.java
@@ -1,17 +1,13 @@
 package com.example.SS2_Backend.dto.request;
 import com.example.SS2_Backend.constants.MessageConst.ErrMessage;
 import com.example.SS2_Backend.constants.MessageConst.ErrCode;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.ValidationResult;
-import org.jfree.util.Log;
 import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
@@ -24,7 +20,8 @@ import java.util.List;
 
 public class NewStableMatchingProblemDTO {
 
-    //TODO: validate String length <= 255, thêm message
+    // TODO: validate String length <= 255, thêm message
+    @Max(value = 255, message = ErrMessage.PROBLEM_NAME)
     private String problemName;
 
     @Min(value = 2, message = ErrMessage.MES_001)
@@ -36,8 +33,8 @@ public class NewStableMatchingProblemDTO {
     @Min(value = 1, message = ErrMessage.MES_003)
     private int numberOfProperty;
 
-    //TODO: làm gì có cái array nào length < 0 mà min 0
-    @Size(min = 0, message = ErrMessage.MES_004)
+    // TODO: làm gì có cái array nào length < 0 mà min 0
+    @Size(min = 1, message = ErrMessage.MES_004)
     private int[] individualSetIndices;
 
     @Size(min = 1, message = ErrMessage.MES_004)
@@ -52,7 +49,8 @@ public class NewStableMatchingProblemDTO {
     @Size(min = 3, message = ErrMessage.MES_002)
     private List<List<Double>> individualProperties;
 
-    //TODO: validate size >=2, validate not empty for each
+    // TODO: validate size >=2, validate not empty for each
+    @Size(min = 2, message = ErrMessage.EVAL_FN_NUM)
     private String[] evaluateFunction;
 
     //TODO: validate not blank
@@ -60,15 +58,17 @@ public class NewStableMatchingProblemDTO {
     private String fitnessFunction;
 
     //TODO: validate <= 1000
+    @Max(value = 1000, message = ErrMessage.POPULATION_SIZE)
     private int populationSize;
 
     //TODO: validate <= 100
+    @Max(value = 100, message = ErrMessage.GENERATION)
     private int generation;
 
     private int maxTime;
 
     //TODO: add message
-    @NotEmpty
+    @NotEmpty(message = ErrMessage.ALGORITHM)
     private String algorithm;
 
     private String distributedCores;
@@ -76,6 +76,11 @@ public class NewStableMatchingProblemDTO {
     public void isEvaluateFunctionValid(BindingResult bindingResult) {
         ArrayList<Boolean> validEvalFunc = new ArrayList<>();
         for (String evaluateFunction: this.getEvaluateFunction()) {
+            if (evaluateFunction.isEmpty()) {
+                bindingResult.rejectValue("evaluateFunction", "", "Empty evaluateFunction(s)");
+                return;
+            }
+
             ExpressionBuilder e = new ExpressionBuilder(evaluateFunction);
             // TODO: không log bừa bãi, chỉ log những bước:
             //  bắt đầu validate,
@@ -83,7 +88,7 @@ public class NewStableMatchingProblemDTO {
             //  bắt đầu xử lý service nếu validate thành công,
             //  xử lý xong request,
             //  gặp lỗi (log cả e để có trace, không dùng e.getMessage())
-            Log.debug("[Evaluate Function] Validating " + evaluateFunction);
+            // Log.debug("[Evaluate Function] Validating " + evaluateFunction);
             for (int i = 1; i <= this.getNumberOfProperty(); i++) {
                 e.variable(String.format("P%d", i)).variable(String.format("W%d", i));
             }
@@ -91,7 +96,7 @@ public class NewStableMatchingProblemDTO {
             Expression expressionValidator = e.build();
             ValidationResult res = expressionValidator.validate();
             validEvalFunc.add(res.isValid());
-            Log.debug("[Evaluate Function] " + evaluateFunction + "validation result: " + res.isValid());
+            // Log.debug("[Evaluate Function] " + evaluateFunction + "validation result: " + res.isValid());
         }
         if (!validEvalFunc.stream().allMatch(e -> true)) {
             bindingResult.rejectValue("evaluateFunction", "",
@@ -103,34 +108,33 @@ public class NewStableMatchingProblemDTO {
     public void is2DArrayValid(BindingResult bindingResult) {
 
         //TODO: tách message & errCode ra thành constant, trường hợp này dùng chung hết một message và error code
-
         if (!bindingResult.hasFieldErrors("individualRequirements")
                 && individualRequirements.size() != numberOfIndividuals) {
-            bindingResult.rejectValue("individualRequirements", ErrCode.INVALID_LENGTH, "Invalid individualRequirements, doesn't match the number of individuals");
+            bindingResult.rejectValue("individualRequirements", ErrCode.INVALID_LENGTH, ErrMessage.INVALID_ARR_SIZE);
         }
 
         //TODO: thêm error code
         if (!bindingResult.hasFieldErrors("individualWeights")
                 && individualWeights.size() != numberOfIndividuals) {
-            bindingResult.rejectValue("individualWeights", "Invalid individualWeights, doesn't match the number of individuals");
+            bindingResult.rejectValue("individualWeights", ErrCode.INVALID_LENGTH, ErrMessage.INVALID_ARR_SIZE);
         }
 
         //TODO: thêm error code
         if (!bindingResult.hasFieldErrors("individualProperties")
                 && individualProperties.size() != numberOfIndividuals) {
-            bindingResult.rejectValue("individualProperties", "Invalid individualProperties, doesn't match the number of individuals");
+            bindingResult.rejectValue("individualProperties", ErrCode.INVALID_LENGTH, ErrMessage.INVALID_ARR_SIZE);
         }
 
         //TODO: thêm error code
         if (!bindingResult.hasFieldErrors("individualSetIndices")
                 && individualSetIndices.length != numberOfIndividuals) {
-            bindingResult.rejectValue("individualSetIndices", "Invalid individualSetIndices, doesn't match the number of individuals");
+            bindingResult.rejectValue("individualSetIndices", ErrCode.INVALID_LENGTH, ErrMessage.INVALID_ARR_SIZE);
         }
 
         //TODO: thêm error code
         if (!bindingResult.hasFieldErrors("individualCapacities")
                 && individualCapacities.length != numberOfIndividuals) {
-            bindingResult.rejectValue("individualCapacities", "Invalid individualCapacities, doesn't match the number of individuals");
+            bindingResult.rejectValue("individualCapacities", ErrCode.INVALID_LENGTH, ErrMessage.INVALID_ARR_SIZE);
         }
 
     }
@@ -144,15 +148,14 @@ public class NewStableMatchingProblemDTO {
         }
 
         boolean isValid = true;
-        String[][] individual2DReq = fromListToStringArray(individualRequirements);
-        double[][] individual2DWeight = fromListToDoubleArray(individualWeights);
-        double[][] individual2DValue = fromListToDoubleArray(individualProperties);
 
         //TODO: tại sao không loop qua list mà phải map về array?
         //TODO: loop qua 3 List<List<T>>, bỏ 2 hàm static đi NẾU không cần thiết phải map về array
-        for (int i = 0; isValid && i < individual2DValue.length; ++i) {
-            isValid = (individual2DValue[i].length == individual2DWeight[i].length)
-                    && (individual2DValue[i].length == individual2DReq[i].length);
+        // REPLY: Em cần phải chuyển nó về 2D Array để xử lý trong bài toán
+        // (Có thể em sẽ thử đưa lại về dạng List<List<T>> xem nếu em xong phần Test)
+        for (int i = 0; isValid && i < individualProperties.size(); ++i) {
+            isValid = (individualProperties.get(i).size() == individualWeights.get(i).size())
+                    && (individualProperties.get(i).size() == individualRequirements.get(i).size());
         }
 
         //TODO: Làm tương tự: thêm err code và message như trên
@@ -160,7 +163,6 @@ public class NewStableMatchingProblemDTO {
             bindingResult.rejectValue("individualRequirements", "", "");
             bindingResult.rejectValue("individualWeights", "", "");
             bindingResult.rejectValue("individualProperties", "", "");
-
         }
     }
 

9e6a795c67acd6df79c67482b3b5c626753c81e1
Manh Nguyen <nguyenphucmanh194@gmail.com>
2024-12-08 00:45:06 +0700

    Update mapper toOTO

--- src/main/java/com/example/SS2_Backend/dto/mapper/StableMatchingProblemMapper.java
+++ src/main/java/com/example/SS2_Backend/dto/mapper/StableMatchingProblemMapper.java
@@ -26,8 +26,32 @@ import com.example.SS2_Backend.util.EvaluatorUtils;
 public class StableMatchingProblemMapper {
 
     public static OTOProblem toOTO(NewStableMatchingProblemDTO dto) {
-        //TODO: implement OTO map logic
-        return null;
+        Requirement[][] requirements = RequirementDecoder.decode(dto.getIndividualRequirements());
+        MatchingData data = new MatchingData(
+                dto.getNumberOfIndividuals(),
+                dto.getNumberOfProperty(),
+                dto.getIndividualSetIndices(),
+                null,
+                dto.getIndividualProperties(),
+                dto.getIndividualWeights(),
+                requirements
+        );
+        data.setExcludedPairs(dto.getExcludedPairs());
+        PreferenceBuilder builder = new TwoSetPreferenceProvider(
+                data,
+                dto.getEvaluateFunctions()
+        );
+        PreferenceListWrapper preferenceLists = builder.toListWrapper();
+        FitnessEvaluator fitnessEvaluator = new TwoSetFitnessEvaluator(data);
+        return new OTOProblem(
+                dto.getProblemName(),
+                dto.getNumberOfIndividuals(),
+                dto.getNumberOfSets(),
+                data,
+                preferenceLists,
+                dto.getFitnessFunction(),
+                fitnessEvaluator
+        );
     }
 
 
